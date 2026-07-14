41e31b7fdbbceb4c53b8c7b08127ef7454a60017
= <dpailahueque01@servel.cl>
2026-07-14 09:29:52 -0400

    chore: bump version to 1.9.7 to fix npm publish conflict

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -15,7 +15,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "qdd",
 	Short:   "QDD (Quality Driven Development) Framework CLI",
-	Version: "v1.9.5",
+	Version: "v1.9.7",
 	Long: `QDD es un CLI para gobernar, generar y evaluar arquitecturas de software aplicando certificaciones de calidad obligatorias.
 garantizando certificaciones, evidencia y calidad desde el día uno.
 
