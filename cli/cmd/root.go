e0fbbc49331c4fa901f2ea627d1c8be61c275bff
T3RRIBL3D4G0 <dpailahueque01@gmail.com>
2026-07-13 21:06:59 -0400

    chore(release): v1.9.2 - Fix NPM publish conflict

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -15,7 +15,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "qdd",
 	Short:   "QDD (Quality-Driven Development) Framework CLI",
-	Version: "v1.9.1",
+	Version: "v1.9.2",
 	Long: `QDD es un CLI para gobernar, generar y evaluar arquitecturas de software aplicando certificaciones de calidad obligatorias.
 garantizando certificaciones, evidencia y calidad desde el día uno.
 
